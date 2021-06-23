b7911f9759d6633b3507460ee3afbb4d01da9350
EGOIST <0x142857@gmail.com>
2021-06-23 21:54:26 +0800

    refactor: update types

--- src/config.ts
+++ src/config.ts
@@ -1,14 +1,24 @@
 import { Plugin as EsbuildPlugin, BuildOptions } from 'esbuild'
 
-export const defineConfig = (config: Config | Config[]) => config
+export const defineConfig = (
+  /**
+   * Multiple configurations = multiple builds
+   */
+  config: Config | Config[],
+) => config
 
 export type Config = InputOptions & {
+  /**
+   * You can also supply multiple output configurations
+   */
   output: OutputOptions | OutputOptions[]
 }
 
 export type InputOptions = {
   /**
    * The context directory
+   *
+   * @default `process.cwd()`
    */
   context?: string
   /**
@@ -17,8 +27,17 @@ export type InputOptions = {
   input?: string | string[]
   plugins?: Plugin[]
   esbuildPlugins?: EsbuildPlugin[]
+  /**
+   * Mark modules as external
+   *
+   * TODO: mark nesting module as external too,
+   * e.g. `external: ['preact']` should also mark `preact/compat` as external.
+   */
   external?: string[]
   watch?: boolean
+  /**
+   * @see https://esbuild.github.io/api/#inject
+   */
   inject?: string[]
   /**
    * Allow JSX without importing `react`
@@ -47,19 +66,31 @@ export type OutputOptions = {
     css?: string
   }
   legalComments?: BuildOptions['legalComments']
+  /**
+   * @see https://esbuild.github.io/api/#out-extension
+   */
   extension?: BuildOptions['outExtension']
 }
 
 type MaybePromise<T> = T | Promise<T>
 
+/**
+ * yaup plugin, modeled after rollup plugin
+ */
 export type Plugin = {
   name: string
 
+  /**
+   * Similar to rollup's `resolveId` hook
+   */
   resolveId?: (
     id: string,
     importer?: string,
   ) => MaybePromise<string | undefined | null | false>
 
+  /**
+   * Similar to rollup's `transform` hook
+   */
   transform?: (
     code: string,
     id: string,
